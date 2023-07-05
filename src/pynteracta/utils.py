c575f047c967ae71de85216aadb6054f0d76eb14
Simone Dalla <simodalla@gmail.com>
2023-07-05 18:44:13 +0200

    refactor dellle classi Settings e spostamento dei modelli e classi per toml e setting di interacta nel pacchetto pynteraccta

--- src/pynteracta/utils.py
+++ src/pynteracta/utils.py
@@ -1,8 +1,16 @@
 import functools
 import json
+import tomllib
 from pathlib import Path
+from typing import Any
 
 from jwt.exceptions import InvalidTokenError
+from pydantic.fields import FieldInfo
+from pydantic_settings import (
+    BaseSettings,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+)
 from requests.models import Response
 
 from .exceptions import InteractaError
@@ -136,3 +144,50 @@ def interactapi(func=None, *, schema_out=None):
         return result
 
     return wrapper
+
+
+class TomlConfigSettingsSource(PydanticBaseSettingsSource):
+    def get_field_value(self, field: FieldInfo, field_name: str) -> tuple[Any, str, bool]:
+        env_file = self.config.get("env_file")
+        with open(env_file, "rb") as ft:
+            file_content_toml = tomllib.load(ft)
+            field_value = file_content_toml.get(field_name)
+            return field_value, field_name, False
+
+    def prepare_field_value(
+        self, field_name: str, field: FieldInfo, value: Any, value_is_complex: bool
+    ) -> Any:
+        return value
+
+    def __call__(self) -> dict[str, Any]:
+        d: dict[str, Any] = {}
+
+        for field_name, field in self.settings_cls.model_fields.items():
+            field_value, field_key, value_is_complex = self.get_field_value(field, field_name)
+            field_value = self.prepare_field_value(field_name, field, field_value, value_is_complex)
+            if field_value is not None:
+                d[field_key] = field_value
+
+        return d
+
+
+class TomlSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file_encoding="utf-8",
+    )
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        return (
+            init_settings,
+            TomlConfigSettingsSource(settings_cls),
+            env_settings,
+            file_secret_settings,
+        )
