efbb583cc3265c7f197453e509b174281efa7932
Simone Dalla <simodalla@gmail.com>
2023-09-14 19:57:44 +0200

    bump version v0.3.2 -> v0.3.3

--- src/pynteracta/__init__.py
+++ src/pynteracta/__init__.py
@@ -1,2 +1,2 @@
 # Version of pynteracta package
-__version__ = "0.3.2"
+__version__ = "0.3.3"
--- src/pynteracta/__main__.py
+++ src/pynteracta/__main__.py
@@ -18,7 +18,7 @@ More information is available at:
 Version:
 --------
 
-- pynteracta v0.3.2
+- pynteracta v0.3.3
 """
 
 from .cli.commands import app
